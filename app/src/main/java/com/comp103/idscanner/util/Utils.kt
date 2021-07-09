638b42ca3b7a57405b7ca5c9647f32d413dc6eba
isaacy2012 <isaacy2012@gmail.com>
2021-07-10 11:53:02 +1200

    Fixed emailAdapter function
    - Used Uri.fromParts and Intent.createChooser
    - Using startActivity

--- app/src/main/java/com/comp103/idscanner/util/Utils.kt
+++ app/src/main/java/com/comp103/idscanner/util/Utils.kt
@@ -1,12 +1,15 @@
 package com.comp103.idscanner.util
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 
 
-val address : String = "theAdmin@nathansoftware.com" // TODO remove test email
-val subject : String = "Test Email From IDScanner App" // TODO update to include useful information including data
+const val address : String = "theAdmin@nathansoftware.com" // TODO remove test email
+const val subject : String = "Test Email From IDScanner App" // TODO update to include useful information including data
 
 /**
  * Email Intent to send the adapter
@@ -15,27 +18,32 @@ val subject : String = "Test Email From IDScanner App" // TODO update to include
  * @param input - list to output
  */
 fun emailAdapter(context : Context, input : List<String>) {
-    val intent = Intent(Intent.ACTION_SENDTO)
-    intent.setData(Uri.parse("mailto:")) // only email apps should handle this
-
-    intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(address))
+    val intent = Intent(
+        Intent.ACTION_SENDTO,
+        Uri.fromParts("mailto", address, null)
+    )
     intent.putExtra(Intent.EXTRA_SUBJECT, subject)
     intent.putExtra(Intent.EXTRA_TEXT, parseInput(input))
-    intent.setType("message/rfc822");
 
-    if (intent.resolveActivity(context.getPackageManager()) != null) {
-        context.startActivity(intent)
+    try {
+        context.startActivity(Intent.createChooser(intent, "Send Email"))
+    } catch (ex: ActivityNotFoundException) {
+        Toast.makeText(
+            context,
+            "There are no email clients installed.",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
 
 /**
  * Parse a list of strings into a single CSV
  *
- * NOTE: this util function is basic for the MVP, but kept in case it is needed to format the output differently in future
+ * NOTE: this util function is basic for the MVP, but kept in case it is needed to format the
+ * output differently in future
  *
  * @param input - list to parse
  */
 fun parseInput(input : List<String>) : String {
-
     return input.joinToString()
 }
