bf3efffbfde9c15b09a845f6cc12303b39ec957c
Frank Prince <theAdmin@nathansoftware.com>
2021-07-10 10:29:43 +1200

    Update Utils.kt

--- app/src/main/java/com/comp103/idscanner/util/Utils.kt
+++ app/src/main/java/com/comp103/idscanner/util/Utils.kt
@@ -1,13 +1,27 @@
 package com.comp103.idscanner.util
 
+import android.content.Intent
+import android.net.Uri
+import androidx.core.content.ContextCompat.startActivity
 import com.comp103.idscanner.itemAdapter.ItemAdapter
 
-public class Utils {
 
+val address : String = "theAdmin@nathansoftware.com" // TODO remove test email
 
-    companion object {
-        fun emailAdapter(adapter : ItemAdapter) {
+/**
+ * Email Intent to send the adapter
+ */
+fun emailAdapter(input : List<String>) {
 
-        }
+}
+
+fun parseInput(input : List<String>) : String {
+
+    val output = StringBuilder()
+
+    for (item in input) {
+        output.append(item)
     }
+
+    return output.toString()
 }
