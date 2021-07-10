479b9f3e988242664e34ac849c5bfdf2571e35c2
Frank Prince <theAdmin@nathansoftware.com>
2021-07-10 17:18:07 +1200

    Updated Intent
    - fixed bug on different API's where body of intent didn't show
    - added date to intent subject

--- app/src/main/java/com/comp103/idscanner/util/Email.kt
+++ app/src/main/java/com/comp103/idscanner/util/Email.kt
@@ -5,12 +5,11 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import com.comp103.idscanner.Id
 
 
 const val address: String = "theAdmin@nathansoftware.com" // TODO remove test email
-const val subject: String = "Test Email From IDScanner App" // TODO update to include useful information including data
+const val subject: String = "[Comp103] IDScanner Participation - " // TODO update to include useful information including data
 
 /**
  * Email Intent to send the adapter
@@ -19,11 +18,18 @@ const val subject: String = "Test Email From IDScanner App" // TODO update to in
  * @param itemList - list to output
  */
 fun sendEmail(context: Context, itemList: List<Id>) {
-    val emailIntent = Intent(Intent.ACTION_SENDTO,
-        Uri.fromParts("mailto", address, null)
-    )
-    emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
-    emailIntent.putExtra(Intent.EXTRA_TEXT, listToEmailString(itemList));
+    // generate a selector to point the intent to ACTION_SENDTO
+    val emailSelectorIntent = Intent(Intent.ACTION_SENDTO)
+    emailSelectorIntent.data = Uri.parse("mailto:")
+
+    // generate intent to contain "EXTRA"s which are the address, subject, body of the email
+    val emailIntent = Intent(Intent.ACTION_SEND)
+
+    emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(address))
+    emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject + getCurrentTime())
+    emailIntent.putExtra(Intent.EXTRA_TEXT, listToEmailString(itemList))
+
+    emailIntent.selector = emailSelectorIntent  // assign selector
 
     try {
         context.startActivity(Intent.createChooser(emailIntent, "Send Email"))
