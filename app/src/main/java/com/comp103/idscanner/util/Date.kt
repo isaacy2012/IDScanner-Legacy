7593bcff656f5614c6a2b59863e2d6bec52e0fcd
isaacy2012 <isaacy2012@gmail.com>
2021-07-11 12:01:23 +1200

    Using system (wall clock) time for date

--- app/src/main/java/com/comp103/idscanner/util/Date.kt
+++ app/src/main/java/com/comp103/idscanner/util/Date.kt
@@ -1,5 +1,5 @@
 /**
- * @author Nathanael Rais
+ * @author Nathanael Rais, Isaac Young
  */
 package com.comp103.idscanner.util
 
@@ -7,9 +7,9 @@ import android.icu.text.SimpleDateFormat
 import java.util.*
 
 fun getCurrentTime() : String {
-    val c: Date = Calendar.getInstance().time
+    val c = Date(System.currentTimeMillis())
     println("Current time => $c")
 
-    val df = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
+    val df = SimpleDateFormat("HH:mm yyyy-MM-dd", Locale.getDefault())
     return df.format(c)
 }
