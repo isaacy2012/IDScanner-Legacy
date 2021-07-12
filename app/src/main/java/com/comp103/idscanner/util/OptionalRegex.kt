eb479a3d13d55d6ddbf3efd98213bf14aa781595
isaacy2012 <isaacy2012@gmail.com>
2021-07-12 13:32:47 +1200

    Changed to OptionalRegex and added documentation

---
+++ app/src/main/java/com/comp103/idscanner/util/OptionalRegex.kt
@@ -0,0 +1,33 @@
+/**
+ * @author Isaac Young
+ */
+package com.comp103.idscanner.util
+
+interface OptionalRegex {
+    /**
+     * @param str The string to match
+     */
+    fun passes(str: String): Boolean
+}
+
+class EnabledRegex(regexString: String) : OptionalRegex {
+    val regex: Regex = Regex(regexString)
+
+    /**
+     * Checks that the string matches the regex
+     * @param str The string to match
+     */
+    override fun passes(str: String): Boolean {
+        return regex.matches(str)
+    }
+}
+
+class DisabledRegex : OptionalRegex {
+    /**
+     * Returns true since matching is disabled
+     * @param str The string to match
+     */
+    override fun passes(str: String): Boolean {
+        return true
+    }
+}
